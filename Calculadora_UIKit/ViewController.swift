418b73f4728088fa441e819484a8cefba56765b3
gabrielaflores4 <gabriela.flores9@catolica.edu.sv>
2026-03-25 21:57:39 -0600

    7

--- Calculadora_UIKit/ViewController.swift
+++ Calculadora_UIKit/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
     var num2: Double = 0
     var operacion: String = ""
     var estaEscribiendo = false
+    var historial: String = ""
 
     @IBAction func botonPresionado(_ sender: UIButton) {
         if let texto = sender.titleLabel?.text {
@@ -35,82 +36,107 @@ class ViewController: UIViewController {
         }
     }
 
-    func manejarOperacion(_ texto: String) {
+func manejarOperacion(_ texto: String) {
 
-        if texto == "+" || texto == "-" || texto == "x" || texto == "/" {
-            
-            if operacion == "" {
-                num1 = Double(resultadoLabel.text!) ?? 0
-            } else if estaEscribiendo {
-                
-                num2 = Double(resultadoLabel.text!) ?? 0
-
-                switch operacion {
-                case "+":
-                    num1 += num2
-                case "-":
-                    num1 -= num2
-                case "x":
-                    num1 *= num2
-                case "/":
-                    if num2 != 0 {
-                        num1 /= num2
-                    } else {
-                        resultadoLabel.text = "Error"
-                        return
-                    }
-                default:
-                    break
-                }
-            }
-
-            operacion = texto
-            resultadoLabel.text = "\(num1)"
-            estaEscribiendo = false
-            return
-        }
-
-        if texto == "=" {
-
-            if estaEscribiendo {
-                num2 = Double(resultadoLabel.text!) ?? 0
+    if texto == "+" || texto == "-" || texto == "x" || texto == "/" {
+        
+        if operacion == "" {
+            if let textoLabel = resultadoLabel.text, let valor = Double(textoLabel) {
+                num1 = valor
+                historial = "\(num1)"
             } else {
-                num2 = num1
+                resultadoLabel.text = "Error"
+                return
+            }
+        } else if estaEscribiendo {
+            
+            if let textoLabel = resultadoLabel.text, let valor = Double(textoLabel) {
+                num2 = valor
+            } else {
+                resultadoLabel.text = "Error"
+                return
             }
 
             switch operacion {
             case "+":
-                resultadoLabel.text = "\(num1 + num2)"
+                num1 += num2
             case "-":
-                resultadoLabel.text = "\(num1 - num2)"
+                num1 -= num2
             case "x":
-                resultadoLabel.text = "\(num1 * num2)"
+                num1 *= num2
             case "/":
-                if num2 == 0 {
+                if num2 != 0 {
+                    num1 /= num2
+                } else {
                     resultadoLabel.text = "Error"
                     return
-                } else {
-                    resultadoLabel.text = "\(num1 / num2)"
                 }
             default:
                 break
             }
-
-            estaEscribiendo = false
-            num1 = Double(resultadoLabel.text!) ?? 0
-            operacion = ""
+            historial += " \(operacion) \(num2)"
         }
 
-        if texto == "C" {
-            resultadoLabel.text = "0"
-            num1 = 0
-            num2 = 0
-            operacion = ""
-            estaEscribiendo = false
-        }
+        operacion = texto
+        historial += " \(texto)"
+        resultadoLabel.text = historial
+        estaEscribiendo = false
+        return
     }
-    
-    override func viewDidLoad() {
+
+    if texto == "=" {
+
+        if estaEscribiendo {
+            if let textoLabel = resultadoLabel.text, let valor = Double(textoLabel) {
+                num2 = valor
+            } else {
+                resultadoLabel.text = "Error"
+                return
+            }
+        } else {
+            num2 = num1
+        }
+
+        historial += " \(num2) ="
+
+        var resultadoFinal: Double = 0
+
+        switch operacion {
+        case "+":
+            resultadoFinal = num1 + num2
+        case "-":
+            resultadoFinal = num1 - num2
+        case "x":
+            resultadoFinal = num1 * num2
+        case "/":
+            if num2 == 0 {
+                resultadoLabel.text = "Error"
+                return
+            } else {
+                resultadoFinal = num1 / num2
+            }
+        default:
+            break
+        }
+
+        resultadoLabel.text = historial + " \(resultadoFinal)"
+
+        estaEscribiendo = false
+        num1 = resultadoFinal
+        operacion = ""
+    }
+
+    if texto == "C" {
+        resultadoLabel.text = "0"
+        num1 = 0
+        num2 = 0
+        operacion = ""
+        estaEscribiendo = false
+        historial = ""
+    }
+}
+
+override func viewDidLoad() {
         super.viewDidLoad()
         resultadoLabel.text = "0"
     }
